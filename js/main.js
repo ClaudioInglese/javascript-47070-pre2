12198365624ad441ef73a1a250ed2b60f94cd1b8
Claudio Inglese <mrcode622@gmail.com>
2023-09-27 01:32:51 -0300

    Para Segunda Preentrega

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// Array de frutas para adivinar
 const frutas = ["manzana", "banana", "naranja", "uva", "pera"];
 
-// Objeto que contiene las imágenes de las frutas
 const frutasImagenes = {
     manzana: "./img/manzana.jpg",
     banana: "./img/banana.jpg",
@@ -10,9 +8,10 @@ const frutasImagenes = {
     pera: "./img/pera.jpg",
 };
 
-// Función de orden superior para iniciar el juego
+
 function jugarADivinarFruta(frutas, frutasImagenes) {
     alert("Bienvenido al juego de adivinanza de frutas. Debes adivinar la fruta secreta.");
+    console.log("Mensaje de Bienvenida");
     return function () {
         let adivinado = false;
 
@@ -24,7 +23,8 @@ function jugarADivinarFruta(frutas, frutasImagenes) {
                 mostrarImagen(frutaAdivinar);                
                 adivinado = true;
                 setTimeout(function () {
-                    alert("¡Felicidades! Adivinaste la fruta!")
+                    alert("¡Felicidades! Adivinaste la fruta! ");
+                    console.log("¡Adivinaste! La fruta es " + frutaAdivinar);
                 }, 50);                
             } else {
                 alert("Esa no es la fruta correcta. Intenta nuevamente.");
@@ -33,20 +33,16 @@ function jugarADivinarFruta(frutas, frutasImagenes) {
     };
 }
 
-// Función para obtener una fruta aleatoria del array de frutas
 function obtenerFrutaAleatoria(frutas) {
     const indice = Math.floor(Math.random() * frutas.length);
     return frutas[indice];
 }
 
-// Función para mostrar la imagen de la fruta adivinada
 function mostrarImagen(fruta) {
     const imagen = document.querySelector("img");
     imagen.src = frutasImagenes[fruta];    
 }
 
-// Llamar a la función de orden superior para iniciar el juego
 const iniciarJuego = jugarADivinarFruta(frutas, frutasImagenes);
 
-// Llamar a la función iniciarJuego al cargar la página
 window.onload = iniciarJuego;
